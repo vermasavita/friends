a3b0ededeb299aff5e2166b42a83bbbcb855d2b1
Savita Verma <svitaverma10@gmail.com>
2022-05-22 11:06:34 +0530

    feat: add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
