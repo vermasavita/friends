8cdc00c808516fada656ee0e9d4415aa16032c40
Savita Verma <svitaverma10@gmail.com>
2022-05-24 17:06:51 +0530

    refactor sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -12,31 +12,31 @@ const Sidebar = () => {
 
       <div className=" flex justify-end  md:justify-between">
         <ul className=" lg:w-4/5 md:flex md:justify-between md:items-center md:m-0 sidebarlist">
-          <li className="py-2 text-xl flex gap-3 items-center px-3 hover:bg-white cursor-pointer rounded-md mb-3 mt-3 md:py-0">
+          <li className="py-2 text-xl flex gap-3 items-center px-3 hover:bg-white list cursor-pointer rounded-md mb-3 mt-3 md:py-0">
             <NavLink to="/">
               <i class="bx bxs-home mr-3"></i>
               <span className="md:hidden">Feed</span>
             </NavLink>
           </li>
-          <li className="py-2 text-xl flex gap-2 items-center px-3 hover:bg-white shadow-current cursor-pointer rounded-md mb-3 md:mb-0">
+          <li className="py-2 text-xl flex gap-2 items-center px-3 hover:bg-white  list  shadow-current cursor-pointer rounded-md mb-3 md:mb-0 md:py-0">
             <NavLink to="/explore">
               <i class="bx bxs-compass mr-3"></i>
               <span className="md:hidden">Explore</span>
             </NavLink>
           </li>
-          <li className="py-2 text-xl flex gap-2 items-center px-3 hover:bg-white shadow-current cursor-pointer rounded-md mb-3 md:mb-0">
+          <li className="py-2 text-xl flex gap-2 items-center px-3 hover:bg-white  list  shadow-current cursor-pointer rounded-md mb-3 md:mb-0 md:py-0">
             <NavLink to="/bookmark">
               <i class="bx bxs-bookmark-star mr-3"></i>
               <span className="md:hidden">Bookmark</span>
             </NavLink>
           </li>
-          <li className="py-2 text-xl flex gap-2 items-center px-3 hover:bg-white shadow-current cursor-pointer rounded-md mb-3 md:mb-0">
+          <li className="py-2 text-xl flex gap-2 items-center px-3 hover:bg-white  list  shadow-current cursor-pointer rounded-md mb-3 md:mb-0 md:py-0">
             <NavLink to="/profile">
               <i class="bx bxs-user-circle mr-3"></i>
               <span className="md:hidden">Profile</span>
             </NavLink>
           </li>
-          <li className="py-2 text-xl flex gap-2 items-center px-3 hover:bg-white shadow-current cursor-pointer rounded-md mb-3 md:mb-0">
+          <li className="py-2 text-xl flex gap-2 items-center px-3 hover:bg-white  list  shadow-current cursor-pointer rounded-md mb-3 md:mb-0 md:py-0">
             <NavLink to="/login">
               <i class="bx bx-log-out mr-3"></i>
               <span className="md:hidden">Logout</span>
@@ -44,7 +44,7 @@ const Sidebar = () => {
           </li>
 
           <li className="py-2 justify-center bg-blue-400 text-white text-xl flex gap-2 items-center px-3 cursor-pointer rounded-md mb-3 md:hidden">
-            Post
+            <button>Post</button>
           </li>
         </ul>
       </div>
