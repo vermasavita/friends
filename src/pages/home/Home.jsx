548d37ade974053201a4319ad1e5361a8cc7ea51
Savita Verma <svitaverma10@gmail.com>
2022-05-24 15:29:31 +0530

    feat: bookmark

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,16 @@
 import { Follow, Search, Sidebar } from "../../components";
 import { AllPost } from "../allPost/AllPost";
-import { SinglePost } from "./components/SinglePost";
+import { Bookmark } from "../bookmark/Bookmark";
 
-const Home = () => {
+const Home = ({children}) => {
   return (
-    <div className="flex bg-slate-100 h-full md:w-full justify-center relative">
+    <div className="flex bg-slate-100 min-h-screen md:w-full justify-center relative">
       <Sidebar />
       <div className="w-2/4 relative md:pr-0 flex flex-col items-center xl:mx-0 lg:w-2/3 pr-5 home">
         <Search />
-        <AllPost/>
+        <div className="flex justify-center"><Bookmark/></div>
+        
+        {/* <AllPost/> */}
       </div>
       <Follow />
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { Login } from "./authentication/Login";
 export { Signup } from "./authentication/Signup";
 export { Home } from "./home/Home";
+// export { BookMark } from "./bookmark/Bookmark";
