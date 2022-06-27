50e10b84432a457054a1b6243e0c5ef83c5447e9
Savita Verma <svitaverma10@gmail.com>
2022-06-28 02:05:18 +0530

    Changed file name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import { store } from "./app/Store";
+import { store } from "./app/store";
 import { Provider } from "react-redux";
 
 // Call make Server
