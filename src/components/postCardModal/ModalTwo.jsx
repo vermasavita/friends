e3c694824bf685506f7bb28cc386fac35d5df589
Savita Verma <svitaverma10@gmail.com>
2022-08-04 11:17:42 +0530

    remove search component

--- src/components/postCardModal/ModalTwo.jsx
+++ src/components/postCardModal/ModalTwo.jsx
@@ -3,10 +3,7 @@ import { openPostCardModal } from "../postCardModal/postCardModalSlice";
 const PostCardModalTwo = () => {
   const dispatch = useDispatch();
   return (
-    <div
-      onClick={() => dispatch(openPostCardModal())}
-      className=" bg-sky-50 flex flex-col w-5/6 ll xl:w-full h-13 top-0 mb-5"
-    >
+      <div onClick={() => dispatch(openPostCardModal())} className="bg-sky-50 flex flex-col w-5/6 ll search xl:w-full h-13 sticky top-0 mb-2">
       <div className="bg-white my-2 py-2 w-full h-10s rounded-md">
         <div className="sm:w-11/12  flex justify-between w-full px-5 items-center">
           <p className="text-slate-400">Write something.....</p>
--- src/pages/home/Wrapper.jsx
+++ src/pages/home/Wrapper.jsx
@@ -5,8 +5,7 @@ const Wrapper = ({ children }) => {
     <div className="flex w-full bg-sky-50 min-h-screen justify-center relative">
       <Sidebar />
       <div className="w-2/4 mb-4 width lg:w-3/5 relative md:pr-0 flex flex-col items-center xl:mx-0 pr-5 md:pb-12">
-        <Search />
-        <PostCardModalTwo/>
+        <PostCardModalTwo />
         <div className="flex justify-center w-full ">{children}</div>
       </div>
       <div className="w-1/4 lg:w-0">
